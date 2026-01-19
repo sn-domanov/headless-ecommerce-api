afc991327f966b461e94d2b53e02a91ccb031a85
Sergei Domanov <sdomanov.py@gmail.com>
2026-01-19 16:02:31 +0100

    feat(auth): add custom user model with migration

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from apps.accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    ordering = ("email",)
+    list_display = ("email", "is_staff", "is_superuser")
+    search_fields = ("email",)
+
+    # Skip user creation and change forms in this project
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("last_login", "date_joined")}),
+    )
--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Authentication
+AUTH_USER_MODEL = "accounts.User"

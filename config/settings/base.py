1c81d2a97c3d87a0d90bb45cde9eac947a7dd643
Sergei Domanov <sdomanov.py@gmail.com>
2026-01-23 12:56:53 +0100

    fix(api): add DRF to INSTALLED_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,7 +48,7 @@ LOCAL_APPS = [
     "apps.products.apps.ProductsConfig",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
